7e505aee91dd621f6331cefe93b1e864bf070d0c
kage <kagepark1@gmail.com>
2020-07-23 15:50:00 -0700

    Change name logging to printf and enhance printf() (marge between logging() and printf() to printf())

--- kmisc.py
+++ kmisc.py
@@ -116,6 +116,9 @@ def log_format(*msg,**opts):
         return m_str
 
 def logging(*msg,**opts):
+    return printf(*msg,**opts)
+
+def printf(*msg,**opts):
     log_p=False
     log=opts.get('log',None)
     log_level=opts.get('log_level',8)
@@ -127,6 +130,14 @@ def logging(*msg,**opts):
     caller_detail=opts.get('caller_detail',False)
     msg=list(msg)
     direct=opts.get('direct',False)
+    color=opts.get('color',None)
+    color_db=opts.get('color_db',{'blue': 34, 'grey': 30, 'yellow': 33, 'green': 32, 'cyan': 36, 'magenta': 35, 'white': 37, 'red': 31})
+    bg_color=opts.get('bg_color',None)
+    bg_color_db=opts.get('bg_color_db',{'cyan': 46, 'white': 47, 'grey': 40, 'yellow': 43, 'blue': 44, 'magenta': 45, 'red': 41, 'green': 42})
+    attr=opts.get('attr',None)
+    attr_db=opts.get('attr_db',{'reverse': 7, 'blink': 5,'concealed': 8, 'underline': 4, 'bold': 1})
+    syslogd=opts.get('syslogd',None)
+
     if direct:
         new_line=opts.get('new_line','')
     else:
@@ -150,22 +161,20 @@ def logging(*msg,**opts):
                     logfile=logfile+logfile_list[1].split(',')
                 msg.remove(ii)
 
-#    def get_caller_fcuntion_name(detail=caller_detail):
-#        try:
-#            dep=len(inspect.stack())-2
-#            if detail:
-#                return '{}({}:{})'.format(sys._getframe(dep).f_code.co_name,sys._getframe(dep).f_lineno,sys._getframe(dep).f_code.co_filename)
-#            else:
-#                name=sys._getframe(dep).f_code.co_name
-#                if name == '_bootstrap_inner' or name == '_run_code':
-#                    return '{}()'.format(sys._getframe(3).f_code.co_name)
-#                return '{}()'.format(name)
-#        except:
-#            return False
+    if os.getenv('ANSI_COLORS_DISABLED') is None and (color or bg_color or attr):
+        reset='''\033[0m'''
+        fmt_msg='''\033[%dm%s'''
+        if color and color in color_db:
+            msg=fmt_msg % (color_db[color],msg)
+        if bg_color and bg_color in bg_color_db:
+            msg=fmt_msg % (color_db[bg_color],msg)
+        if attr and attr in attr_db:
+            msg=fmt_msg % (attr_db[attr],msg)
+        msg=msg+reset
 
     # Make a Intro
     intro_msg=''
-    if date:
+    if date and syslogd is False:
         intro_msg='[{0}] '.format(datetime.now().strftime(date_format))
     if caller:
         call_name=get_caller_fcuntion_name(detail=caller_detail)
@@ -176,10 +185,12 @@ def logging(*msg,**opts):
                 intro_msg=intro_msg+'{}(): '.format(call_name)
     if intro is not None:
         intro_msg=intro_msg+intro+': '
+
     # Make a Tap
     tap=''
     for ii in range(0,len(intro_msg)):
         tap=tap+' '
+
     # Make a msg
     msg_str=''
     for ii in msg:
@@ -190,6 +201,24 @@ def logging(*msg,**opts):
                 msg_str=msg_str+'{}'.format(ii)
         else:
             msg_str=intro_msg+'{}'.format(ii)
+
+    # save msg to syslogd
+    if syslogd:
+        if syslogd in ['INFO','info']:
+            syslog.syslog(syslog.LOG_INFO,msg)
+        elif syslogd in ['KERN','kern']:
+            syslog.syslog(syslog.LOG_KERN,msg)
+        elif syslogd in ['ERR','err']:
+            syslog.syslog(syslog.LOG_ERR,msg)
+        elif syslogd in ['CRIT','crit']:
+            syslog.syslog(syslog.LOG_CRIT,msg)
+        elif syslogd in ['WARN','warn']:
+            syslog.syslog(syslog.LOG_WARNING,msg)
+        elif syslogd in ['DBG','DEBUG','dbg','debug']:
+            syslog.syslog(syslog.LOG_DEBUG,msg)
+        else:
+            syslog.syslog(msg)
+
     # Save msg to file
     if type(logfile) is str:
         logfile=logfile.split(',')
