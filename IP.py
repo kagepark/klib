26ed49da809fa619a796ed19a6786ac016ee368f
Kage Park <kagepark1@gmail.com>
2021-03-14 15:04:02 -0700

    Add new file

---
+++ IP.py
@@ -0,0 +1,110 @@
+#Kage Park
+from klib.kmisc import *
+
+class IP:
+    def __init__(self,src):
+        self.src=src
+
+    def IsV4(self,ip=None):
+        if ip is None:
+            ip=self.src
+        if isinstance(ip,str):
+            ipa = ip.strip().split(".")
+            if len(ipa) != 4: return False
+            for ipn in ipa:
+                if not ipn.isdigit() or not 0 <= int(ipn) <= 255: return False
+            return True
+        return False
+
+    def WithPort(self,port,**opts):
+        default=opts.get('default',False)
+        tcp_sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tcp_sk.settimeout(1)
+        if self.IsV4(self.src) is False or not isinstance(port,(str,int,list,tuple)):
+            return default
+        if isinstance(port,(str,int)):
+            try:
+                port=[int(port)]
+            except:
+                return default
+        for pt in port:
+            try:
+                tcp_sk.connect((self.src,pt))
+                return True
+            except:
+                pass
+        return False
+
+    def Ip2Num(self,ip=None,default=False):
+        if ip is None:
+            ip=self.src
+        if isinstance(ip,int):
+            return ip
+        if self.IsV4(ip):
+            return struct.unpack("!L", socket.inet_aton(ip))[0]
+        return default
+
+    def InRange(self,start_ip,end_ip,**opts):
+        default=opts.get('default',False)
+        startip=self.Ip2Num(start_ip)
+        myip=self.Ip2Num(self.src)
+        endip=self.Ip2Num(end_ip)
+        if isinstance(startip,int) and isinstance(myip,int) and isinstance(endip,int):
+            if startip <= myip <= endip: return True
+            return False
+        return default
+
+    def LostNetwork(self,**opts):
+        default=opts.get('default',False)
+        timeout_sec=opts.get('timeout',1800)
+        interval=opts.get('interval',2)
+        keep_good=opts.get('keep_good',30)
+        cancel_func=opts.get('cancel_func',None)
+        log=opts.get('log',None)
+        init_time=None
+        if self.IsV4():
+            if not ping(self.src,count=3):
+                if not ping(self.src,count=0,timeout=timeout_sec,keep_good=keep_good,interval=interval,cancel_func=cancel_func,log=log):
+                    return True
+            return False
+        return default
+
+    def Online(self,**opts):
+        default=opts.get('default',False)
+        timeout_sec=opts.get('timeout',1800)
+        interval=opts.get('interval',3)
+        keep=opts.get('keep',20)
+        cancel_func=opts.get('cancel_func',None)
+        log=opts.get('log',None)
+        init_time=None
+        run_time=int_sec()
+        if self.IsV4(self.src):
+            if log:
+                log('[',direct=True,log_level=1)
+            while True:
+                ttt,init_time=timeout(timeout_sec,init_time)
+                if ttt:
+                    if log:
+                        log(']\n',direct=True,log_level=1)
+                    return False,'Timeout monitor'
+                if self.cancel(cancel_func):
+                    if log:
+                        log(']\n',direct=True,log_level=1)
+                    return True,'Stopped monitor by Custom'
+                if ping(self.src,cancel_func=cancel_func):
+                    if (int_sec() - run_time) > keep:
+                        if log:
+                            log(']\n',direct=True,log_level=1)
+                        return True,'OK'
+                    if log:
+                        log('-',direct=True,log_level=1)
+                else:
+                    run_time=int_sec()
+                    if log:
+                        log('.',direct=True,log_level=1)
+                time.sleep(interval)
+            if log:
+                log(']\n',direct=True,log_level=1)
+            return False,'Timeout/Unknown issue'
+        return default,'IP format error'
---
+++ MAC.py
@@ -0,0 +1,26 @@
+#Kage Park
+class MAC:
+    def __init__(self,src):
+        self.src=src
+
+    def IsV4(self):
+        symbol=opts.get('symbol',':')
+        default=opts.get('default',False)
+        if isinstance(self.src,str):
+            self.src=self.src.strip()
+            # make sure the format
+            if 12 <= len(self.src) <= 17:
+                for i in [':','-']:
+                    self.src=self.src.replace(i,'')
+                self.src=symbol.join(self.src[i:i+2] for i in range(0,12,2))
+            # Check the normal mac format
+            octets = self.src.split(symbol)
+            if len(octets) != 6: return False
+            for i in octets:
+                try:
+                   if len(i) != 2 or int(i, 16) > 255:
+                       return False
+                except:
+                   return False
+            return True
+        return default
